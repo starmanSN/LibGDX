2a27a33acdc4d144f5612504996d6812b8a3be60
StarmanSN <nimaevs@gmail.com>
2022-09-18 23:23:01 +0300

    Homework3

---
+++ core/src/com/mygdx/game/Physics.java
@@ -0,0 +1,29 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Physics {
+    final World world;
+    private final Box2DDebugRenderer debugRenderer;
+
+    public Physics() {
+        world = new World(new Vector2(0, -9.81f), true);
+        debugRenderer = new Box2DDebugRenderer();
+    }
+
+    public void debugDraw(OrthographicCamera camera) {
+        debugRenderer.render(world, camera.combined);
+    }
+
+    public void step() {
+        world.step(1 / 60f, 3, 3);
+    }
+
+    public void dispose() {
+        world.dispose();
+        debugRenderer.dispose();
+    }
+}
--- core/src/com/mygdx/game/animation/MyAtlasAnimation.java
+++ core/src/com/mygdx/game/animation/MyAtlasAnimation.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.animation;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -9,12 +11,19 @@ public class MyAtlasAnimation {
     TextureAtlas atlas;
     Animation<TextureAtlas.AtlasRegion> animation;
     private float time;
+    private Sound sound;
+    private boolean loop;
+    private float d;
 
-    public MyAtlasAnimation(String atlas, String name, float fps, Animation.PlayMode playMode) {
+    public MyAtlasAnimation(String atlas, String name, float fps, boolean playMode, String sound) {
+        if (playMode) loop = true;
+        this.sound = Gdx.audio.newSound(Gdx.files.internal(sound));
+        this.sound.play();
         time = 0;
         this.atlas = new TextureAtlas(atlas);
         animation = new Animation<>(1 / fps, this.atlas.findRegions(name));
-        animation.setPlayMode(playMode);
+        animation.setPlayMode(Animation.PlayMode.NORMAL);
+        d = animation.getAnimationDuration() / 2;
     }
 
     public TextureRegion startAnimation() {
@@ -23,10 +32,19 @@ public class MyAtlasAnimation {
 
     public void setTime(float dT) {
         time += dT;
+        if (time > d && time < animation.getAnimationDuration()) {
+            sound.play();
+            d *= 2;
+        } else if (time >= animation.getAnimationDuration() && loop) {
+            time = 0;
+            d = animation.getAnimationDuration() / 2;
+            sound.play();
+        }
     }
 
     public void dispose() {
         this.atlas.dispose();
+        sound.dispose();
     }
 
 }
